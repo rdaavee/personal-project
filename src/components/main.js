26916b49dc59583d89668d5bd2212aaa81b272db
Ranier Arcega <rdavee0412@gmail.com>
2023-11-04 13:27:40 +0800

    portfolio section and scrollbar added

---
+++ src/components/main.js
@@ -0,0 +1,8 @@
+/* ========== SCROLL REVEAL ANIMATION ========== */
+const sr = ScrollReveal( {
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+     // reset: true // the animations will repeat
+})
